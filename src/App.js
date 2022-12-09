8f3ff28484e6a250f8bd0cbc695979461246564f
veektrie <102025110+veektrie@users.noreply.github.com>
2022-12-09 12:01:02 +0100

    navbar created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
 import { Cta, Brand, Navbar } from './components';
+import './App.css'; 
 
 const App = () => {
   return (
